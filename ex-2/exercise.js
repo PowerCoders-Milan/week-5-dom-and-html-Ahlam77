289774ed53937ad89ad37aca14d4963d65d69e0d
ahlam alshaba <ama1977ln@outlook.com>
2021-03-02 15:25:47 +0100

    trying

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,8 +1,5 @@
 function getAttributes() {
     var target = document.getElementById("pwcdr").target;
-    document.getElementById("pwcdr").innerHTML = target;
-    var hreflang = document.getElementById("pwcdr").hreflang;
-    document.getElementById("pwcdr").innerHTML = hreflang;
-    var rel = document.getElementById("pwcdr").rel;
-    document.getElementById("pwcdr").innerHTML = rel;
+    document.getElementById("pwcdr").target = _self;
+    
     }   
