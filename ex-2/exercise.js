32b3bd39ae9cb3b383b5c216d50ff4e5817381bd
ahlam alshaba <ama1977ln@outlook.com>
2021-03-02 15:08:18 +0100

    what

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,8 @@
 function getAttributes() {
-var link = document.querySelectorAll("p")[0]
-}
+    var target = document.getElementById("pwcdr").target;
+    document.getElementById("pwcdr").innerHTML = target;
+    var hreflang = document.getElementById("pwcdr").hreflang;
+    document.getElementById("pwcdr").innerHTML = hreflang;
+    var rel = document.getElementById("pwcdr").rel;
+    document.getElementById("pwcdr").innerHTML = rel;
+    }   
--- ex-5/exercise.js
+++ ex-5/exercise.js
@@ -3,7 +3,9 @@ function loadBooks(array) {
 }
 
 // Paste here your array
-
+var books = [
+    { title: 'The Design of EveryDay Things',  author: 'Don Norman', alreadyRead: false }, { title: 'The power of now', author: 'Ekhart Tolle', alreadyRead: true }, { title: 'The Three Body Problem', author: 'Liu Cixin', alreadyRead: true }
+  ];
 
 // This function will execute when the window loads
 window.onload = loadBooks(array);
