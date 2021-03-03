99ea91cf2b7e09677cef31bba33a89d96a78ada0
ahlam alshaba <ama1977ln@outlook.com>
2021-03-03 14:32:08 +0100

    trying

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,44 +1,8 @@
-var books = [
-    { title: 'The Design of EveryDay Things',  author: 'Don Norman', alreadyRead: false , img : ""},
-    { title: 'The power of now', author: 'Ekhart Tolle', alreadyRead: true , img: ""}, 
-    { title: 'The Three Body Problem', author: 'Liu Cixin', alreadyRead: true, img:"ls" },
-  ];
-
-
-
-  books.forEach(function(item){
-      let titleTag = document.createElement("p");
-      let b = document.createElement("p");
-      let x = document.createTextNode(item.title);
-      let y = document.createTextNode(item.author);
-      let image = document.createElement("img");
-      image.setAttribute("src",item.img);
-      titleTag.appendChild(x);
-      b.appendChild(y);
-    //   document.getElementsByTagName("body")[0].appendChild(titleTag);
-    //   document.getElementsByTagName("body")[0].appendChild(b);
-
-      item.alreadyRead == true ?  titleTag.style.backgroundColor = "yellow": titleTag.style.backgroundColor = "red"
-     var bookList = document.createElement("ul");
-     var listItem1 = document.createElement("li");
-     var listItem2 = document.createElement("li");
-     var listItem3 = document.createElement("li");
-     listItem1.appendChild(titleTag);
-     listItem2.appendChild(b);
-     listItem3.appendChild(image);
-     bookList.appendChild(listItem1);
-     bookList.appendChild(listItem2);
-     bookList.appendChild(listItem3);
-     document.getElementsByTagName("body")[0].appendChild(bookList);
-
-     //console.log(bookList.createElement("li"));
-
-
-
-
-    //   if(item.alreadyRead == true){
-    //       titleTag.style.backgroundColor = "yellow";
-    //   } else {
-    //     titleTag.style.backgroundColor = "red";
-    //   }
-  })
+function getAttributes() {
+    var target = document.getElementById("pwcdr").getAttribute("taregt");
+    var hreflang = document.getElementById("pwcdr").getAttribute("hreflang");
+    var rel = document.getElementById("pwcdr").getAttribute("rel");
+    document.getElementById("pwcdr").innerHTML =` The value of target attribute of the link is: ${e}`;
+    document.getElementById("pwcdr").innerHTML = `The value of hreflang attribute of the link is: ${c}`;
+    document.getElementById("pwcdr").innerHTML = `The value of rel attribute of the link is: ${d}`;
+    }   
