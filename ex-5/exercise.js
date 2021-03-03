2f66303a71d2323bf717becb03ca7d7062e133cf
ahlam alshaba <ama1977ln@outlook.com>
2021-03-03 14:29:18 +0100

    added code  live codes

--- ex-5/exercise.js
+++ ex-5/exercise.js
@@ -1,11 +1,44 @@
-function loadBooks(array) {
-
-}
-
-// Paste here your array
 var books = [
-    { title: 'The Design of EveryDay Things',  author: 'Don Norman', alreadyRead: false }, { title: 'The power of now', author: 'Ekhart Tolle', alreadyRead: true }, { title: 'The Three Body Problem', author: 'Liu Cixin', alreadyRead: true }
-  ];
+  { title: 'The Design of EveryDay Things',  author: 'Don Norman', alreadyRead: false , img : ""},
+  { title: 'The power of now', author: 'Ekhart Tolle', alreadyRead: true , img: ""}, 
+  { title: 'The Three Body Problem', author: 'Liu Cixin', alreadyRead: true, img:"ls" },
+];
 
-// This function will execute when the window loads
-window.onload = loadBooks(array);
+
+
+books.forEach(function(item){
+    let titleTag = document.createElement("p");
+    let b = document.createElement("p");
+    let x = document.createTextNode(item.title);
+    let y = document.createTextNode(item.author);
+    let image = document.createElement("img");
+    image.setAttribute("src",item.img);
+    titleTag.appendChild(x);
+    b.appendChild(y);
+  //   document.getElementsByTagName("body")[0].appendChild(titleTag);
+  //   document.getElementsByTagName("body")[0].appendChild(b);
+
+    item.alreadyRead == true ?  titleTag.style.backgroundColor = "yellow": titleTag.style.backgroundColor = "red"
+   var bookList = document.createElement("ul");
+   var listItem1 = document.createElement("li");
+   var listItem2 = document.createElement("li");
+   var listItem3 = document.createElement("li");
+   listItem1.appendChild(titleTag);
+   listItem2.appendChild(b);
+   listItem3.appendChild(image);
+   bookList.appendChild(listItem1);
+   bookList.appendChild(listItem2);
+   bookList.appendChild(listItem3);
+   document.getElementsByTagName("body")[0].appendChild(bookList);
+
+   //console.log(bookList.createElement("li"));
+
+
+
+
+  //   if(item.alreadyRead == true){
+  //       titleTag.style.backgroundColor = "yellow";
+  //   } else {
+  //     titleTag.style.backgroundColor = "red";
+  //   }
+})
